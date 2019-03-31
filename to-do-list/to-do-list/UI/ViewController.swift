d8f4058028a3d81d2648576a730effc892022891
Maks Niagolov <nyagolovm@gmail.com>
2019-03-31 23:16:02 +0900

    scaffolded the app

---
+++ to-do-list/to-do-list/UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  to-do-list
+//
+//  Created by Maks Niagolov on 31/03/2019.
+//  Copyright © 2019 Maksim Niagolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
